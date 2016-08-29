9992eb7b3371ebbb2636627f2ed46a3de42ac590
Aaron Barany <akb825@gmail.com>
2016-08-28 22:47:27 -0700

    WIP for sampler and render states.

--- Compile/src/Parser.h
+++ Compile/src/Parser.h
@@ -19,6 +19,7 @@
 #include <MSL/Config.h>
 #include "TokenList.h"
 #include <array>
+#include <float.h>
 
 namespace msl
 {
@@ -33,6 +34,10 @@ public:
 		RemoveUniformBlocks = 0x1
 	};
 
+	static const unsigned int maxAttachments = 16;
+
+	static const std::uint32_t unsetInt = 0xFFFFFFFF;
+
 	enum class Stage
 	{
 		Vertex,
@@ -45,11 +50,262 @@ public:
 
 	static const unsigned int stageCount = static_cast<unsigned int>(Stage::Compute) + 1;
 
+	enum class Bool
+	{
+		Unset = -1,
+		False,
+		True
+	};
+
+	enum class Filter
+	{
+		Unset = -1,
+		Nearest,
+		Linear
+	};
+
+	enum class PolygonMode
+	{
+		Unset = -1,
+		Fill,
+		Line,
+		Point
+	};
+
+	enum class CullMode
+	{
+		Unset = -1,
+		Front,
+		Back,
+		Both
+	};
+
+	enum class FrontFace
+	{
+		Unset = -1,
+		CounterClockwise,
+		Clockwise
+	};
+
+	enum class StencilOp
+	{
+		Unset = -1,
+		Keep,
+		Zero,
+		Replace,
+		IncrementAndClamp,
+		DecrementAndClamp,
+		Invert,
+		IncrementAndWrap,
+		DecrementAndWrap
+	};
+
+	enum class CompareOp
+	{
+		Unset = -1,
+		Never,
+		Less,
+		Equal,
+		LessOrEqual,
+		Greater,
+		NotEqual,
+		GreaterOrEqual,
+		Always
+	};
+
+	enum class BlendFactor
+	{
+		Unset = -1,
+		Zero,
+		One,
+		SrcColor,
+		OneMinusSrcColor,
+		DstColor,
+		OneMinusDstColor,
+		SrcAlpha,
+		OneMinusSrcAlpha,
+		DstAlpha,
+		OneMinusDstAlpha,
+		ConstColor,
+		OneMinusConstColor,
+		ConstAlpha,
+		OneMinusConstAlpha,
+		SrcAlphaSaturate,
+		Src1Color,
+		OneMinusSrc1Color,
+		Src1Alpha,
+		OneMinusSrc1Alpha
+	};
+
+	enum class BlendOp
+	{
+		Unset = -1,
+		Add,
+		Subtract,
+		ReverseSubtract,
+		Min,
+		Max
+	};
+
+	enum ColorMask
+	{
+		ColorMaskUnset = -1,
+		ColorMaskNone = 0,
+		ColorMaskRed = 0x1,
+		ColorMaskGreen = 0x2,
+		ColorMaskBlue = 0x4,
+		ColorMaskAlpha = 0x8
+	};
+
+	enum class LogicOp
+	{
+		Unset = -1,
+		Clear,
+		And,
+		AndReverse,
+		Copy,
+		AndInverted,
+		NoOp,
+		Xor,
+		Or,
+		Nor,
+		Equivalent,
+		Invert,
+		OrReverse,
+		CopyInverted,
+		OrInverted,
+		Nand,
+		Set
+	};
+
+	enum class MipFilter
+	{
+		Unset = -1,
+		None,
+		Nearest,
+		Linear,
+		Anisotropic
+	};
+
+	enum class AddressMode
+	{
+		Unset = -1,
+		Repeat,
+		MirroredRepeat,
+		ClampToEdge,
+		ClampToBorder,
+		MirrorOnce
+	};
+
+	enum class BorderColor
+	{
+		Unset = -1,
+		TransparentBlack,
+		OpaqueBlack,
+		OpaqueWhite
+	};
+
+	struct RasterizationState
+	{
+		Bool depthClampEnable = Bool::Unset;
+		Bool rasterizerDiscardEnable = Bool::Unset;
+		PolygonMode polygonMode = PolygonMode::Unset;
+		CullMode cullMode = CullMode::Unset;
+		FrontFace frontFace = FrontFace::Unset;
+		Bool depthBiasEnable = Bool::Unset;
+		float depthBiasConstantFactor = FLT_MAX;
+		float depthBiasClamp = FLT_MAX;
+		float depthBiasSlopeFactor = FLT_MAX;
+		float lineWidth = FLT_MAX;
+	};
+
+	struct MultisampleState
+	{
+		Bool sampleShadingEnable = Bool::Unset;
+		float minSampleShading = FLT_MAX;
+		std::uint32_t sampleMask = unsetInt;
+		Bool alphaToCoverageEnable = Bool::Unset;
+		Bool alphaToOneEnable = Bool::Unset;
+	};
+
+	struct StencilOpState
+	{
+		StencilOp failOp = StencilOp::Unset;
+		StencilOp passOp = StencilOp::Unset;
+		StencilOp depthFailOp = StencilOp::Unset;
+		CompareOp compareOp = CompareOp::Unset;
+		std::uint32_t compareMask = unsetInt;
+		std::uint32_t writeMask = unsetInt;
+		std::uint32_t reference = unsetInt;
+	};
+
+	struct DepthStencilState
+	{
+		Bool depthTestEnable = Bool::Unset;
+		Bool depthWriteEnable = Bool::Unset;
+		CompareOp depthCompareOp = CompareOp::Unset;
+		Bool depthBoundsTestEnable = Bool::Unset;
+		Bool stencilTestEnable = Bool::Unset;
+		StencilOpState frontStencil;
+		StencilOpState backStencilOp;
+		float minDepthBounds = FLT_MAX;
+		float maxDepthBounds = FLT_MAX;
+	};
+
+	struct BlendAttachmentState
+	{
+		Bool blendEnable = Bool::Unset;
+		BlendFactor srcBlendFactor = BlendFactor::Unset;
+		BlendFactor dstColorBlendFactor = BlendFactor::Unset;
+		BlendOp colorBlendOp = BlendOp::Unset;
+		BlendFactor srcAlphaBlendFactor = BlendFactor::Unset;
+		BlendFactor dstAlphaBlendFactor = BlendFactor::Unset;
+		BlendOp alphaBlendOp = BlendOp::Unset;
+		ColorMask colorWriteMask = ColorMaskUnset;
+
+	};
+
+	struct BlendState
+	{
+		Bool logicalOpEnable = Bool::Unset;
+		LogicOp logicalOp = LogicOp::Unset;
+		std::array<BlendAttachmentState, maxAttachments> blendAttachments;
+		std::array<float, 4> blendConstants = {{FLT_MAX, FLT_MAX, FLT_MAX, FLT_MAX}};
+	};
+
+	struct RenderState
+	{
+		RasterizationState rasterizationState;
+		MultisampleState multisampleState;
+		DepthStencilState depthStencilState;
+		BlendState blendState;
+		std::uint32_t patchControlPoints = unsetInt;
+	};
+
 	struct Pipeline
 	{
 		const Token* token;
 		std::string name;
 		std::array<std::string, stageCount> entryPoints;
+		RenderState renderState;
+	};
+
+	struct Sampler
+	{
+		const Token* token;
+		std::string name;
+		Filter minFilter = Filter::Unset;
+		Filter magFilter = Filter::Unset;
+		MipFilter mipFilter = MipFilter::Unset;
+		AddressMode addressModeU = AddressMode::Unset;
+		AddressMode addressModeV = AddressMode::Unset;
+		AddressMode addressModeW = AddressMode::Unset;
+		float mipLodBias = FLT_MAX;
+		Bool anisotropicFiltering = Bool::Unset;
+		float maxAnisotropy = FLT_MAX;
+		float minLod = FLT_MAX;
+		float maxLod = FLT_MAX;
+		BorderColor borderColor = BorderColor::Unset;
 	};
 
 	struct LineMapping
